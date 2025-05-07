ab5fa45eae2673e85f603bbe0aa957a60427307b
Alex Gursky <agursky@pesachtikvah.org>
2025-05-07 10:03:54 -0400

    added typing to main demo

--- ccbhc_measurements/demos/main_demo.py
+++ ccbhc_measurements/demos/main_demo.py
@@ -1,3 +1,5 @@
+from typing import List
+from ccbhc_measurements.abstractions.measurement import Measurement
 from ccbhc_measurements.measurements.dep_rem import Dep_Rem
 from ccbhc_measurements.measurements.asc import ASC
 from ccbhc_measurements.measurements.sdoh import SDOH
@@ -5,6 +7,7 @@ from ccbhc_measurements.demos.dep_rem_demo import data as dr_sub_1_data
 from ccbhc_measurements.demos.asc_sub_1_demo import data as asc_sub_1_data
 from ccbhc_measurements.demos.sdoh_demo import data as sdoh_sub_1_data
 
+measurements : List[Measurement]
 measurements = [
     Dep_Rem(dr_sub_1_data),
     ASC(asc_sub_1_data),
